15f5f9c96d24ef842d5ef00f26ef66e4465e12e3
mathlete01 <msallin@gmail.com>
2020-11-16 17:09:38 -0800

    commit tree c30e4b451d09a0ee1689ae1d83ff7d2bcbbdadc8

--- index.js
+++ index.js
@@ -6,32 +6,36 @@ function destructivelyAppendDriver(name) {
 }
 
 function destructivelyPrependDriver(name) {
-    drivers.unshift(name)
+    drivers.unshift(name);
 }
 
 function destructivelyRemoveLastDriver() {
-    drivers.pop()
+    drivers.pop();
 }
 
 function destructivelyRemoveFirstDriver() {
-    drivers.shift()
+    drivers.shift();
 }
 
 function appendDriver(name) {
     //const otherdriver = [...drivers, name];
    // const otherdriver = drivers.slice(name)
-    const otherdriver = drivers.slice()
-    otherdriver.push(name)
+    const otherdriver = drivers.slice();
+    otherdriver.push(name);
+    return otherdriver;
 }
 
 function prependDriver(name) {
     const otherdriver = [name, ...drivers];
+    return otherdriver;
 }
 
 function removeLastDriver() {
-    drivers.splice(-1)
+    drivers.splice(-1);
+    return drivers;
 }
 
 function removeFirstDriver() {
-    drivers.splice(0)
+    drivers.splice(0);
+    return drivers;
 }
